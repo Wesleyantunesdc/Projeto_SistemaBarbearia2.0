206059347c424a6310f7cc60b2529d8681ae69fe
Wesleyantunesdc <wesleyantunesdca@gmail.com>
2022-08-23 20:37:58 -0300

    Projeto criado

---
+++ backend/barbeariaws/src/main/java/br/com/devantunes/barbeariaws/BarbeariawsApplication.java
@@ -0,0 +1,13 @@
+package br.com.devantunes.barbeariaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariawsApplication.class, args);
+	}
+
+}
---
+++ backend/barbeariaws/src/test/java/br/com/devantunes/barbeariaws/BarbeariawsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devantunes.barbeariaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
